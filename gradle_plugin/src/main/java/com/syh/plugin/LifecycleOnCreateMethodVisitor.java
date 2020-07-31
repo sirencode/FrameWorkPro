cab4a9e5d6390f1b12b70bf4bc1781443ef1b6b8
Diablo <shenyonghe525@gmail.com>
2020-07-31 18:42:00 +0800

    生命周期 ams 实现

---
+++ gradle_plugin/src/main/java/com/syh/plugin/LifecycleOnCreateMethodVisitor.java
@@ -0,0 +1,39 @@
+package com.syh.plugin;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+/**
+ * Created by shenyonghe on 2020/7/31.
+ */
+class LifecycleOnCreateMethodVisitor extends MethodVisitor {
+    public LifecycleOnCreateMethodVisitor(MethodVisitor mv) {
+        super(Opcodes.ASM4, mv);
+    }
+
+    @Override
+    public void visitCode() {
+        //方法执行前插入
+        mv.visitLdcInsn("TAG");
+        mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder");
+        mv.visitInsn(Opcodes.DUP);
+        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
+        mv.visitLdcInsn("-------> onCreate : ");
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+        mv.visitVarInsn(Opcodes.ALOAD, 0);
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Object", "getClass", "()Ljava/lang/Class;", false);
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Class", "getSimpleName", "()Ljava/lang/String;", false);
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC, "android/util/Log", "i", "(Ljava/lang/String;Ljava/lang/String;)I", false);
+        mv.visitInsn(Opcodes.POP);
+
+        super.visitCode();
+        //方法执行后插入
+    }
+
+    @Override
+    public void visitInsn(int opcode) {
+        super.visitInsn(opcode);
+    }
+}
