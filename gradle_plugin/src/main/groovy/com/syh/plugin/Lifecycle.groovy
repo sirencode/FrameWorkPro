8d250721abfe6e2741f6661f48f829457d0505ee
Diablo <shenyonghe525@gmail.com>
2020-08-04 16:07:19 +0800

    transform 注释

--- gradle_plugin/src/main/groovy/com/syh/plugin/Lifecycle.groovy
+++ gradle_plugin/src/main/groovy/com/syh/plugin/Lifecycle.groovy
@@ -27,16 +27,30 @@ class Lifecycle extends Transform implements Plugin<Project> {
         return "LifecyclePlugin"
     }
 
+    // 输入文件类型
     @Override
     Set<QualifiedContent.ContentType> getInputTypes() {
         return TransformManager.CONTENT_CLASS
     }
 
+    /**
+     * 指Transform要操作内容的范围，官方文档Scope有7种类型：
+     * EXTERNAL_LIBRARIES        只有外部库
+     * PROJECT                   只有项目内容
+     * PROJECT_LOCAL_DEPS        只有项目的本地依赖(本地jar)
+     * PROVIDED_ONLY             只提供本地或远程依赖项
+     * SUB_PROJECTS              只有子项目
+     * SUB_PROJECTS_LOCAL_DEPS   只有子项目的本地依赖项(本地jar)
+     * TESTED_CODE               由当前变量(包括依赖项)测试的代码
+     * SCOPE_FULL_PROJECT        整个项目
+     * @return
+     */
     @Override
     Set<? super QualifiedContent.Scope> getScopes() {
         return TransformManager.SCOPE_FULL_PROJECT
     }
 
+    // 指明当前Transform是否支持增量编译
     @Override
     boolean isIncremental() {
         return false
@@ -55,7 +69,9 @@ class Lifecycle extends Transform implements Plugin<Project> {
     void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
         println '--------------- LifecyclePlugin visit start --------------- '
         def startTime = System.currentTimeMillis()
+        // inputs中是传过来的输入流，其中有两种格式，一种是jar包格式一种是目录格式
         Collection<TransformInput> inputs = transformInvocation.inputs
+        // 获取到输出目录，最后将修改的文件复制到输出目录，这一步必须做不然编译会报错
         TransformOutputProvider outputProvider = transformInvocation.outputProvider
         //删除之前的输出
         if (outputProvider != null)
