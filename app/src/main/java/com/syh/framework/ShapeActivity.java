9c8bb76cd83c327df190ac9478a8c6e2426cf9a8
Diablo <shenyonghe525@gmail.com>
2020-10-29 16:46:33 +0800

    shape

---
+++ app/src/main/java/com/syh/framework/ShapeActivity.java
@@ -0,0 +1,24 @@
+package com.syh.framework;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import cn.mtjsoft.www.shapeview.ShapeTextView;
+
+public class ShapeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shape_activity_main);
+
+        ShapeTextView shapeTextView = findViewById(R.id.stv);
+        // 代码动态设置
+        shapeTextView.setSolidColor(ContextCompat.getColor(getBaseContext(), R.color.colorAccent));
+        shapeTextView.setRadius(10);
+        // 设置生效
+        shapeTextView.setCustomBackground();
+    }
+}
---
+++ shapeview/src/main/java/cn/mtjsoft/www/shapeview/ShapeImageView.java
@@ -0,0 +1,109 @@
+package cn.mtjsoft.www.shapeview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+import cn.mtjsoft.www.shapeview.util.GradientDrawableUtil;
+
+/**
+ * 实现自定义圆角背景
+ */
+public class ShapeImageView extends AppCompatImageView {
+
+    //自定背景边框Drawable
+    private GradientDrawable gradientDrawable;
+    //填充色
+    private int solidColor = 0;
+    //边框色
+    private int strokeColor = 0;
+    //边框宽度
+    private int strokeWidth = 0;
+    //四个角的弧度
+    private int radius;
+    private int topLeftRadius;
+    private int topRightRadius;
+    private int bottomLeftRadius;
+    private int bottomRightRadius;
+
+    public ShapeImageView(Context context) {
+        this(context, null);
+    }
+
+    public ShapeImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ShapeImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+        setCustomBackground();
+    }
+
+    /**
+     * 初始化参数
+     *
+     * @param context
+     * @param attrs
+     */
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ShapeImageView);
+        // 填充以及边框
+        solidColor = ta.getColor(R.styleable.ShapeTextView_solidColor, Color.TRANSPARENT);
+        strokeColor = ta.getColor(R.styleable.ShapeTextView_strokeColor, Color.TRANSPARENT);
+        strokeWidth = ta.getDimensionPixelSize(R.styleable.ShapeTextView_strokeWidth, 0);
+        //四个角单独设置会覆盖radius设置
+        radius = ta.getDimensionPixelSize(R.styleable.ShapeTextView_radius, 0);
+        topLeftRadius = ta.getDimensionPixelSize(R.styleable.ShapeTextView_topLeftRadius, radius);
+        topRightRadius = ta.getDimensionPixelSize(R.styleable.ShapeTextView_topRightRadius, radius);
+        bottomLeftRadius = ta.getDimensionPixelSize(R.styleable.ShapeTextView_bottomLeftRadius, radius);
+        bottomRightRadius = ta.getDimensionPixelSize(R.styleable.ShapeTextView_bottomRightRadius, radius);
+        ta.recycle();
+    }
+
+    public void setSolidColor(int solidColor) {
+        this.solidColor = solidColor;
+    }
+
+    public void setStrokeColor(int strokeColor) {
+        this.strokeColor = strokeColor;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setTopLeftRadius(int topLeftRadius) {
+        this.topLeftRadius = topLeftRadius;
+    }
+
+    public void setTopRightRadius(int topRightRadius) {
+        this.topRightRadius = topRightRadius;
+    }
+
+    public void setBottomLeftRadius(int bottomLeftRadius) {
+        this.bottomLeftRadius = bottomLeftRadius;
+    }
+
+    public void setBottomRightRadius(int bottomRightRadius) {
+        this.bottomRightRadius = bottomRightRadius;
+    }
+
+    public void setCustomBackground() {
+        //默认背景
+        gradientDrawable = GradientDrawableUtil.init().getNeedDrawable(new float[]{topLeftRadius, topLeftRadius, topRightRadius, topRightRadius,
+                        bottomRightRadius, bottomRightRadius, bottomLeftRadius, bottomLeftRadius},
+                solidColor, strokeWidth, strokeColor);
+        this.setBackground(gradientDrawable);
+        this.setFocusable(false);
+        this.setFocusableInTouchMode(false);
+    }
+}
